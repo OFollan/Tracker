22d6b441ce1aa3ab99c71584f4db913766f298a6
Øyvind Follan <ofollan@gmail.com>
2020-10-31 08:39:30 +0100

    woop vi er online

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Her kan vi dytte inn litt klatredata
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://klatreverket.no/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Klatreverket
         </a>
       </header>
     </div>
